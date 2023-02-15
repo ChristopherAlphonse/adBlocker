f27f228388421e39117750921692dcd840ce9353
ChristopherAlphonse <christopheralphonse96@gmail.com>
2023-02-15 13:00:52 -0500

    parsing text file

--- background.js
+++ background.js
@@ -1,33 +1,36 @@
 const defaultFilters = [
   "*://*.doubleclick.net/*",
   "*://partner.googleadservices.com/*",
-  "*://*.googlesyndication.com/*",
-  "*://*.google-analytics.com/*",
-  "*://creative.ak.fbcdn.net/*",
-  "*://*.adbrite.com/*",
-  "*://*.exponential.com/*",
-  "*://*.quantserve.com/*",
-  "*://*.scorecardresearch.com/*",
-  "*://*.zedo.com/*",
-  "*://*.googleadservices.com/*",
-  "*://*.doubleclick.net/*",
-  "*://*.googlesyndication.com/*",
-  "*://*.googletagservices.com/*",
-  "*://*.googleadservices.com/*",
-  "*://*.google-analytics.com/*",
-  "*://*.googleadservices.com/*",
-  "*://*.zedo.com/*",
-  "*://*.adbrite.com/*",
-  "*://*.adbureau.net/*",
-  "*://*.carbonads.net/*",
-  "*://*.cdn.carbonads.com/*",
-  "*://*.cdn.carbonads.net/*",
-  "*://*.cdn.doubleclick.net/*",
-  "*://*.cdn.googletagservices.com/*",
-  "*://*.cdn.googleadservices.com/*",
-  "*://*.cdn.zedo.com/*",
-  "*://*.clickup.com/*",
 ];
+
+const filesToFetch = [
+  "easylist.txt",
+  "easyprivacy.txt",
+  "fanboy-annoyance.txt",
+  "fanboy-cookiemonster.txt",
+  "fanboy-social.txt",
+];
+
+Promise.all(filesToFetch.map(fetchFile))
+  .then((fileContents) => {
+    // Parse the filters from each file and add them to the defaultFilters array
+    fileContents.forEach((fileContent) => {
+      const additionalFilters = fileContent
+        .split("\n")
+        .filter((filter) => filter.trim() !== "");
+      defaultFilters.push(...additionalFilters);
+    });
+  })
+  .catch((error) => console.error(error));
+
+async function fetchFile(fileName) {
+  try {
+    const response = await fetch(fileName);
+    return await response.text();
+  } catch (error) {
+    return console.error(error);
+  }
+}
 
 chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
