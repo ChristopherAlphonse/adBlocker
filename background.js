959fdf63c10b4690d4d4a8daf0f0a365f43069c9
ChristopherAlphonse <christopheralphonse96@gmail.com>
2023-02-15 04:25:07 -0500

    looking for more links

--- background.js
+++ background.js
@@ -9,6 +9,23 @@ const defaultFilters = [
   "*://*.quantserve.com/*",
   "*://*.scorecardresearch.com/*",
   "*://*.zedo.com/*",
+  "*://*.doubleclick.net/*",
+  "*://*.googlesyndication.com/*",
+  "*://*.googletagservices.com/*",
+  "*://*.googleadservices.com/*",
+  "*://*.google-analytics.com/*",
+  "*://*.googleadservices.com/*",
+  "*://*.zedo.com/*",
+  "*://*.adbrite.com/*",
+  "*://*.adbureau.net/*",
+  "*://*.carbonads.net/*",
+  "*://*.cdn.carbonads.com/*",
+  "*://*.cdn.carbonads.net/*",
+  "*://*.cdn.doubleclick.net/*",
+  "*://*.cdn.googletagservices.com/*",
+  "*://*.cdn.googleadservices.com/*",
+  "*://*.cdn.zedo.com/*",
+  "*://*.clickup.com/*",
 ];
 
 chrome.webRequest.onBeforeRequest.addListener(
